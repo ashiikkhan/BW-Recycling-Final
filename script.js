6f98a0c6852c1578ab01938099fc7eb94cb1e451
ashiikkhan <ashiksust06@gmail.com>
2022-06-30 18:36:06 +0600

    positioned

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const formDisplay = () => {
-  const btn = document.getElementById('btn');
-  btn.classList.add('d-none');
   const form = document.getElementById('form');
-  form.classList.toggle('form-active');
+  form.classList.add('form-active');
 };
 const closingButton = document.getElementById('closing-btn');
 const form = document.getElementById('form');
-const btn = document.getElementById('btn');
 closingButton.addEventListener('click', () => {
   form.classList.remove('form-active');
-  btn.classList.remove('d-none');
 });
