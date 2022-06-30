67319d733e5ee496981f6a2bca848c7058a223d7
ashiikkhan <ashiksust06@gmail.com>
2022-06-30 17:52:50 +0600

    closing button added

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const formDisplay = () => {
+  const btn = document.getElementById('btn');
+  btn.classList.add('d-none');
   const form = document.getElementById('form');
   form.classList.toggle('form-active');
 };
+const closingButton = document.getElementById('closing-btn');
+const form = document.getElementById('form');
+const btn = document.getElementById('btn');
+closingButton.addEventListener('click', () => {
+  form.classList.remove('form-active');
+  btn.classList.remove('d-none');
+});
