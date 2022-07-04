fc53bc506a99e1c064ddf3b651492cc5c06e48a4
ashiikkhan <ashiksust06@gmail.com>
2022-07-04 15:50:16 +0600

    had to change folder structures

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// form display
+const form = document.getElementById('form');
 const formDisplay = () => {
-  const form = document.getElementById('form');
   form.classList.add('form-active');
 };
+// form closing
 const closingButton = document.getElementById('closing-btn');
-const form = document.getElementById('form');
 closingButton.addEventListener('click', () => {
   form.classList.remove('form-active');
 });
