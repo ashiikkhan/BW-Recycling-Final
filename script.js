ae78f974964b57395203304844da9ca462138b63
ashiikkhan <ashiksust06@gmail.com>
2022-08-05 14:37:41 +0600

    keyboard padding

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ const closingButton = document.getElementById('closing-btn');
 closingButton.addEventListener('click', () => {
   form.classList.remove('form-active');
 });
+
+document.body.addEventListener(
+  'focus',
+  (event) => {
+    const target = event.target;
+    switch (target.tagName) {
+      case 'INPUT':
+      case 'TEXTAREA':
+      case 'SELECT':
+        document.body.classList.add('keyboard');
+    }
+  },
+  true
+);
+document.body.addEventListener(
+  'blur',
+  () => {
+    document.body.classList.remove('keyboard');
+  },
+  true
+);
