294266fc0a251c1c195ef9af0ce4f37ec347d6df
ashiikkhan <ashiksust06@gmail.com>
2022-08-07 18:19:57 +0600

    focus form activated

--- script.js
+++ script.js
@@ -7,4 +7,21 @@ const formDisplay = () => {
 const closingButton = document.getElementById('closing-btn');
 closingButton.addEventListener('click', () => {
   form.classList.remove('form-active');
+  form.classList.remove('focus-form');
 });
+
+// form input focus :
+
+document.body.addEventListener(
+  'focus',
+  (event) => {
+    const target = event.target;
+    switch (target.tagName) {
+      case 'INPUT':
+      case 'TEXTAREA':
+      case 'SELECT':
+        form.classList.add('focus-form');
+    }
+  },
+  true
+);
